975df88776a810755a1fea1e43aa82a8e0812660
Aarebecca <943720372@qq.com>
2018-12-24 22:10:24 +0800

    Signed-off-by: Aarebecca <943720372@qq.com>

--- manage.py
+++ manage.py
@@ -2,18 +2,28 @@ from app import create_app
 from flask_script import Manager
 import os
 from flask_migrate import MigrateCommand
+
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+
+
 # 从环境变量中获取config_name
 config_name = os.environ.get('FLASK_CONFIG') or 'default'
 
 # 生成app
 app = create_app(config_name)
 
+http_server = HTTPServer(WSGIContainer(app))
+
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    manager.run()
+    http_server.listen(5000)
+    IOLoop.instance().start()
+    # manager.run()
 
 
 
